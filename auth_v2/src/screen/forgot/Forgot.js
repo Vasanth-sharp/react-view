0a7d38a656f1e239e9614edc45a4fd5641d5d0e8
Vasanthakumar <118686684+Vasanth-sharp@users.noreply.github.com>
2024-03-24 17:22:24 +0530

    Update Forgot.js

--- auth_v2/src/screen/forgot/Forgot.js
+++ auth_v2/src/screen/forgot/Forgot.js
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 import Loader from "react-spinner-loader";
 
+
 export default function Forgot() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
